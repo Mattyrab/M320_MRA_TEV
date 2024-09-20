bb756c065a63add4e9e43327c8c8b1d893f53727
Matthew <129963345+Mattyrab@users.noreply.github.com>
2024-09-20 09:44:05 +0200

    feature: add body_size attribute to Goalie

--- M320-V2/src/main/java/Goalie.java
+++ M320-V2/src/main/java/Goalie.java
@@ -1,6 +1,16 @@
 public class Goalie extends Player{
 
+    private double body_size = 0;
+
+    public void setBody_size(double body_size) {
+        this.body_size = body_size;
+    }
+
+    public double getBody_size() {
+        return body_size;
+    }
+
     public void play() {
-        System.out.println("Protect the goal");
+        System.out.println("Protect the goal.");
     }
 }
