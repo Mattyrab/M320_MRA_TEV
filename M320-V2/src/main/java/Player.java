c9800a7fba216e1cc57d2c96f2defb27e143051a
Matthew <129963345+Mattyrab@users.noreply.github.com>
2024-09-20 09:30:38 +0200

    feature: add attributes for Team class and Player class, add getter and setter method for Player name

--- M320-V2/src/main/java/Player.java
+++ M320-V2/src/main/java/Player.java
@@ -1,2 +1,12 @@
 public class Player {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- M320-V2/src/main/java/Team.java
+++ M320-V2/src/main/java/Team.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class Team {
+
+    private ArrayList<Attacker> attackers = new ArrayList<>();
+    private ArrayList<Defender> defenders = new ArrayList<>();
+    private Goalie goalie;
 }
