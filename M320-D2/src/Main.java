78cd28c13e101ad80e67c22dc19e47e6111f28a8
Matthew <129963345+Mattyrab@users.noreply.github.com>
2024-09-20 09:07:04 +0200

    fix: rename class object and add it to constructor of students

--- M320-D2/src/Main.java
+++ M320-D2/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Class schoolClass = new Class("Math Class");
+        Class school1 = new Class("Math Class");
 
-        Student student1 = new Student("Student1");
-        Student student2 = new Student("Student2");
-        Student student3 = new Student("Student3");
+        Student student1 = new Student("Student1", school1);
+        Student student2 = new Student("Student2", school1);
+        Student student3 = new Student("Student3", school1);
 
         student1.addTest(new Test(80, 100)); // Added maxPoints parameter
         student2.addTest(new Test(80, 100));
@@ -18,15 +18,15 @@ public class Main {
         student2.addTest(new Test(100, 100));
         student3.addTest(new Test(100, 100));
 
-        schoolClass.addStudent(student1);
-        schoolClass.addStudent(student2);
-        schoolClass.addStudent(student3);
+        school1.addStudent(student1);
+        school1.addStudent(student2);
+        school1.addStudent(student3);
 
-        for (Student student : schoolClass.getStudents()) {
+        for (Student student : school1.getStudents()) {
             System.out.println("Average Grade for " + student.getName() + ": " +
                     student.getAverageGrade());
         }
 
-        System.out.println("Class Average: " + schoolClass.getAverage());
+        System.out.println("Class Average: " + school1.getAverage());
     }
 }
